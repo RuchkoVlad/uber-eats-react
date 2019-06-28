3b09751cfaf0ab6af36147d1f944b659eb0bf7d7
Vlad Ruchko <necessarydp@gmail.com>
2019-06-28 17:18:46 +0300

    #5 issues fixed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,15 +8,15 @@ export function Card(props) {
             <a href="" className=" card card__link">
                 <img className="card__img" src={props.imageUrl} alt="food_picture"/>
                 <span className="card__restaurant">{props.title}</span>
-                    <span className="card__category" >{props.priceBucket} • {props.categories.map((category) => {
-                        return (
-                            category.keyName + ' • '
+                <span className="card__category">{props.priceBucket} • {props.categories.map((category) => {
+                    return (
+                        category.keyName + ' • '
                     )
-                    })}
+                })}
                     </span>
 
                 <span className="card__time">{props.etaRange.min} - {props.etaRange.max} Min</span>
             </a>
         </li>
-        );
+    );
 }
