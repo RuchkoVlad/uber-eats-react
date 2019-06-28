da8dcf7aa17843835556341705d513d2067384a4
Vlad Ruchko <necessarydp@gmail.com>
2019-06-28 17:20:29 +0300

    #6 issues fixed

--- src/components/CabinetButton/CabinetButton.jsx
+++ src/components/CabinetButton/CabinetButton.jsx
@@ -3,8 +3,8 @@ import './CabinetButton.css';
 import basket from "./img/basketbasket.svg";
 
 export function CabinetButton() {
-    return (<div className="cabinet-button__wrapper">
-        <button className="cabinet-button">Sign In</button>
+    return (<div className="cabinet-button">
+        <button className="cabinet-button__wrapper">Sign In</button>
         <button className="cabinet-button cabinet-button--black">Register</button>
         <a href="https://www.ubereats.com/ru-UA/checkout/">
             <img src={basket} alt="basket"/>
