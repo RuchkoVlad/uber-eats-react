ebd67d3f14f3d9564f2615d5de50b5a5335788d4
Vlad Ruchko <necessarydp@gmail.com>
2019-06-28 17:12:14 +0300

    #2 issues fixed

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,6 +4,6 @@ import './Search.css';
 export function Search() {
     return(
         <label className="search__label">
-            <input type="text" className="search" placeholder="Search for restaurant or cuisine"/>
+            <input type="text" className="search__input" placeholder="Search for restaurant or cuisine"/>
         </label>);
 }
