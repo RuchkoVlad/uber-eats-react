314995003b4d59621c547725f01517b2a118b270
Vlad Ruchko <necessarydp@gmail.com>
2019-07-12 16:27:44 +0300

    add favicon

--- src/components/Choose/Choose.jsx
+++ src/components/Choose/Choose.jsx
@@ -13,22 +13,22 @@ export function Choose() {
         <span className="choose__title">Kyiv Restaurants</span>
 
         <ul className="choose__cards">
-            <div className="choose__grid">
-          {restaurants.map(restaurant => {
-            return (
-              <Link to="/restaurant" className="choose__router-link">
-                <Card
-                  key={restaurant.uuid}
-                  title={restaurant.title}
-                  categories={restaurant.categories}
-                  priceBucket={restaurant.priceBucket}
-                  etaRange={restaurant.etaRange}
-                  imageUrl={restaurant.imageUrl}
-                />
-              </Link>
-            );
-          })}
-            </div>
+          <div className="choose__grid">
+            {restaurants.map(restaurant => {
+              return (
+                <Link to="/restaurant" className="choose__router-link">
+                  <Card
+                    key={restaurant.uuid}
+                    title={restaurant.title}
+                    categories={restaurant.categories}
+                    priceBucket={restaurant.priceBucket}
+                    etaRange={restaurant.etaRange}
+                    imageUrl={restaurant.imageUrl}
+                  />
+                </Link>
+              );
+            })}
+          </div>
         </ul>
       </div>
     </div>
